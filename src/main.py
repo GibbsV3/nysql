7a571a4762e588c2e35eac7ba4d35e900f08f051
Nidhish <nyarlagadda@sensorworx.com>
2026-09-25 16:12:24 -0400

    feat: Very basic scaffolding for main

--- src/main.py
+++ src/main.py
@@ -3,3 +3,14 @@ Todo: Start [WHATEVER ITS CALLED] in main and expose a port on the local machine
 other applicaitions use to talk to the database. Call it what? Its like an api gateway. We can call it
 Communication managerx
 """
+
+def main():
+    #We will do something here.
+    raise NotImplementedError('We need to still implement main')
+
+
+
+if __name__ == '__main__':
+    main()
+
+
