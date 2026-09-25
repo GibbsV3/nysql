8f733be0d707b1948954be2d8e3a21dda12c70a8
Nidhish <nyarlagadda@sensorworx.com>
2026-09-24 22:01:49 -0400

    docs: Add scaffold documentation for Communication Manager

---
+++ docs/architecture.md
@@ -0,0 +1,4 @@
+# Architecture
+
+## Communication Manger
+A port needs to be exposed so that other applications can make requests and receive responses. The Communication Manger will handle this and directing traffic inside the database engine.
---
+++ src/main.py
@@ -0,0 +1,5 @@
+"""
+Todo: Start [WHATEVER ITS CALLED] in main and expose a port on the local machine. This will be what
+other applicaitions use to talk to the database. Call it what? Its like an api gateway. We can call it
+Communication managerx
+"""
